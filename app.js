b91bd16296cd08c5e83fb57ef65702844cb56ae5
Jan Michael Jose <janmichael.jose@gmail.com>
2020-07-22 23:04:40 +0800

    created new app for destiny-stats (boilerplate)

---
+++ app.js
@@ -0,0 +1,23 @@
+var ex = require("express");
+var ej = require("ejs");
+var mongoose = require("mongoose");
+var app = ex();
+var bP = require("body-parser"); 
+var methodOverride = require("method-override");
+var sanitizer = require("express-sanitizer");
+var port = process.env.PORT || 3000;
+const request = require('request');
+var editMode=0;
+app.use(bP.urlencoded({extended: true}));
+app.use(ex.static("public"));
+app.use(methodOverride("_method"));
+app.use(sanitizer());
+
+
+app.get("/", function (req, res) {
+   res.render("index.ejs");
+  });
+
+app.listen(port, function () {
+    console.log("Server is running");
+  });
